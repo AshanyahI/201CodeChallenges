fd51f21ca26d166670a74f0c11ee849526db8524
AshanyahI <bangiebbb@gmail.com>
2023-02-14 05:45:12 +0000

    Print the Loggin History

---
+++ codechallenge03.sh
@@ -0,0 +1,26 @@
+
+
+#!/bin/bash
+
+# Script: Ops 201 Class 03 Ops Challenge Solution
+# Author: Ashanyah Israel
+# Date of latest revision: 2/13/23
+# Purpose: Write a script that prints the login history of users on this computer.
+
+
+# Main
+#Write a script that prints the login history of users on this computer.
+#Your script must use at least one function and one variable.
+
+printHistory (){
+    echo "Welcome to the loggin history"
+
+}
+saysMe="done"
+
+printHistory
+last
+
+echo $saysMe
+# End
+
