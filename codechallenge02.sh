2169bdb07abe097ece556810e8ae9f971eb800db
AshanyahI <bangiebbb@gmail.com>
2023-02-14 04:55:38 +0000

    Print the string to the terminal

--- codechallenge02.sh
+++ codechallenge02.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Script: Ops 201 Class 02 Ops Challenge Solution
-# Author:
-# Date of latest revision:
+# Author: Ashanyah Israel
+# Date of latest revision: 2/13/23
 # Purpose: Print a string to the terminal
 
 
 # Main
-
+echo My pronoun is Israelite Princess
 # End
