7a4b12f12592cb822e7c9dbcf12962553bd5c88b
AshanyahI <124545448+AshanyahI@users.noreply.github.com>
2024-01-25 09:41:40 -0800

    Update codechallenge02.sh

--- codechallenge02.sh
+++ codechallenge02.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Script: Ops 201 Class 02 Ops Challenge Solution
+# Script: Ops SP 201 Class 02 Ops My First Bash Sript
 # Author: Ashanyah Israel
 # Date of latest revision: 1/25/24
 # Purpose: Print a string to the terminal
 
 
 # Main
-echo I'm working Perfectly
+echo I am working Perfectly!
 # End
